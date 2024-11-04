f4cee4b99e4869827cc62c1eb7e806012487cf36
LeeJinHee <wlsgml8847@kakao.com>
2024-11-04 21:16:53 +0900

    [Add] radin

---
+++ leet-code/week-1/03-two-pointers/1/radin.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        txt = str(s)
+        newStr = ''.join(filter(str.isalnum, txt)).lower()
+
+        if newStr.count == 0:
+            return true
+        else:
+            st = 0
+            ed = len(newStr) - 1
+            isPalindrome = True
+            while st < ed:
+                if newStr[st] == newStr[ed]:
+                    st += 1
+                    ed -= 1
+                else:
+                    isPalindrome = False
+                    break
+            
+            return isPalindrome
+            
+            
+            
