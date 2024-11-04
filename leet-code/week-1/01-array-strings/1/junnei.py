007dee4c69f348e72f5686c32855d60ea4544265
Seongjun Jang <peinguin77@gmail.com>
2024-11-04 17:27:59 +0900

    [Add] Week-1/01-Rotate-Image/junnei

--- leet-code/week-1/01-array-strings/1/junnei.py
+++ leet-code/week-1/01-array-strings/1/junnei.py
@@ -1,21 +1,19 @@
-# 변수 선언 및 입력
-n = int(input())
+class Solution:
+    def rotate(self, matrix: list[list[int]]) -> None:
+        n = len(matrix)
+        for x in range(n//2):
+            for y in range((n+1)//2):
+                matrix = self.rotate_elements(matrix, x, y, n)
+    
+    def rotate_elements(self, matrix: list[list[int]], x: int, y: int, n: int) -> list[list[int]]:
+        temp = matrix[x][y]
+        for _ in range(4):
+            x, y = self.get_pos(x, y, n)
+            temp, matrix[x][y] = matrix[x][y], temp
+        return matrix
 
-# 동적 배열 선언
-v = list()
-
-01-array-string
-
-for _ in range(n):
-    command = input()
-
-    if command.startswith("push_back"):
-        _, num = tuple(command.split())
-        v.append(int(num))
-    elif command.startswith("pop_back"):
-        v.pop()
-    elif command.startswith("size"):
-        print(len(v))
-    else:
-        _, index = tuple(command.split())
-        print(v[int(index) - 1])
+    def get_pos(self, x: int, y: int, n: int) -> tuple[int, int]:
+        center = (n-1)/2
+        dx, dy = center - x, center - y
+        nx, ny = center - dy, center + dx
+        return int(nx), int(ny)
